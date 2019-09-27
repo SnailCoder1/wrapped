375d595cfd3a489af55c988481ca4027c92970d8
Boa Matule <boa.matule@gmail.com>
2019-09-27 03:39:44 +0200

    Adds a new image for the tech stack in the footer; Adds more data to the projects json file. WIP

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => {
         <a href="https://tailwindcss.com/" target="_blank"><img src="./src/img/tailwind.png" height="31" width="31" alt="Tailwind Logo"></img></a>
         <a href="https://reactjs.org/" target="_blank"><img src="./src/img/reactjs.png" height="31" width="31" alt="React Logo"></img></a>
         <a href="https://www.cypress.io/" target="_black"><img src="./src/img/cypress.png" height="31" width="31" alt="Cypress Logo"></img></a>
-        <a href="http://rspec.info/" target="_black"><img src="./src/img/rspec.png" height="31" width="31" alt="RSpec Logo"></img></a>
+        <a href="http://rspec.info/" target="_black"> <img src="./src/img/rspec.png" height="31" width="31" alt="RSpec Logo"></img></a>
+        <a href="https://devcenter.heroku.com/" target="_black"><img src="./src/img/heroku.png" height="31" width="31" alt="Heroku Logo"></img></a>
         <a href="https://cucumber.io/" target="_black"><img src="./src/img/cucumber.png" height="31" width="31" alt="Cucumber Logo"></img></a>
         <a href="https://redux.js.org/" target="_black"><img src="./src/img/redux.png" height="31" width="31" alt="Redux Logo"></img></a>
         <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_black"><img src="./src/img/HTML.png" height="31" width="31" alt="Html Logo"></img></a>
