9b87f86730bcd4880d5e8358f1a82ef6177d9024
Boa Matule <boa.matule@gmail.com>
2019-09-30 16:10:10 +0200

    cleans up the app.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -12,10 +12,9 @@ const Contact = () => {
             <h1 className="w-full text-center  md:w-1/2 px-3 mb-10 md:mb-3">Get in Touch</h1>
 
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              {/* <input type="hidden" name="form-name" value="contact" /> */}
-              <input className="block w-full bg-white-200 text-gray-700 border border-gray py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="Your name" name="name" required />
-              <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="email" placeholder="Your email" name="email" required />
-              <textarea className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray py-16 px-4 mb-3 py-4 leading-tight focus:outline-none focus:bg-white" placeholder="Your message" name="message" required />
+              <input className="block w-full bg-white-200 text-gray-700 border border-gray py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type='text' placeholder="Your name" name='name' required />
+              <input className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="email" placeholder="Your email" name='email' required />
+              <textarea className="appearance-none block w-full bg-white-200 text-gray-700 border border-gray py-16 px-4 mb-3 py-4 leading-tight focus:outline-none focus:bg-white" placeholder="Your message" name='message' required />
               <button className="appearance-none block w-full bg-white-200 text-gray-700 border border-indigo py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:bg-indigo">Send your message</button>
             </div>
           </>
